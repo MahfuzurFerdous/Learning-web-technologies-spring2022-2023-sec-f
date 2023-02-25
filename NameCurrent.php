d0958a536eb1d38b33c34faca8f77eab7b50c50c
MahfuzurFerdous <88179454+MahfuzurFerdous@users.noreply.github.com>
2023-02-26 01:29:08 +0600

    Add files via upload

---
+++ NameCurrent.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>name</title>
+    </head>
+
+    <body>
+        
+        <form method="POST" action="NameHandler.php">
+        <fieldset>
+            <legend>Name</legend> 
+            <input type="text" name="name" value=""/> <br>
+            <input type="submit" name="" value="Submit">
+        </fieldset>
+        </form>
+    </body>
+</html>
---
+++ dobCurrent.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>dob</title>
+    <head>
+        <body>
+            <form method="POST" action="dobHandler.php">
+                <fieldset>
+                    <legend>Date Of Birth</legend>
+                    <input type="date" name="dob" value=""/></br>
+                    <input type="submit" name="submit" value="Submit"/>
+                </fieldset>
+            </form>
+        </body>
+    </head>
+</html>
+
+
